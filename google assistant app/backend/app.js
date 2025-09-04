d405c7ed6b6fdda974e8254fb18e18ed21e67709
Nikunj Jain <jainnikunj170@gmail.com>
2025-09-04 14:42:50 +0530

    Update app.js

--- google assistant app/backend/app.js
+++ google assistant app/backend/app.js
@@ -13,5 +13,9 @@ app.use('/api/news', newsRoutes);
 app.use('/api/music', musicRoutes);
 app.use('/api/user', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Google Assistant Backend is Running!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
